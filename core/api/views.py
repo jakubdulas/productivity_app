e480096c6ba18880512db1a40c5f2699121cb01d
Jakub Dulas <70340102+jakubdulas@users.noreply.github.com>
2022-11-15 12:37:14 +0100

    added authenticated only permission

--- core/api/views.py
+++ core/api/views.py
@@ -1,19 +1,22 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Task, Category, Event
 from .serializers import TaskSerializer, CategorySerializer, EventSerializer
 # Create your views here.
 
 # Authentication
 
-@api_view(['POST'])
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def home(request):
     print(request.user)
     return Response({'text': 'Hello world!'})
 
-@api_view()
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_tasks(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user).all()
     serialied_tasks = TaskSerializer(tasks, many=True)
 
     data  = {
@@ -22,36 +25,20 @@ def get_tasks(request):
 
     return Response(data)
 
-@api_view()
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_categories(request):
-    categories = Category.objects.all()
+    categories = Category.objects.filter(user=request.user).all()
     serializer = CategorySerializer(categories, many=True)
     data  = serializer.data
     return Response(data)
 
-@api_view()
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_events(request):
-    events = Event.objects.all()
+    events = Event.objects.filter(user=request.user).all()
     serialized_events = EventSerializer(events, many=True)
     data = {
         "events": serialized_events.data
     }
     return Response(data)
-
-# {
-#  "events": 
-#  [
-#   {
-#     "id": 1,
-#     "name": "urodziny przemka",
-#     "category": "school",
-#     "date": "01.01.2001",
-#   },
-#   {
-#     "id": 2,
-#     "name": "imieniny zbyszka",
-#     "category": "home",
-#     "date": "05.01.2001",
-#   },
-#  ]
-# }
